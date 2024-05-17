4fbe7d91a924941487207c363c5b3560270fc16e
Gwparsa <Gwparsa@gmail.com>
2024-05-17 19:44:17 +0330

    updates on typing content

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let captionLength = 0;
-let caption = '';
-let nowDate = new Date();
+let caption;
+let status;
+let discordStatus;
 
 $(document).ready(function() {
     setInterval ('cursorAnimation()', 600);
@@ -9,9 +10,21 @@ $(document).ready(function() {
 });
 
 function testTypingEffect() {
-    caption = `${nowDate}<br><br>Parsa Sorsori - Software Engineer`; //input text here
-    type();
-}
+    statusChanger();
+    caption = `Parsa Sorsori<br> Software Engineer<br><span class='discordStatus'>${status}</span><br><br><br>Hello there! My name is Parsa and I am a 20-year-old engineer from Iran. My interests lie in the field of computers and technologies, which is why I am currently studying Computer Engineering at QIAU. I discovered my passion for programming at the age of 11 and since then, I have been learning the C# language. At 16, I began exploring front-end web development and UI designing.<br><br><br><a href='http://gwparsa.ir/projects'>/Projects</a><a href='http://gwparsa.ir/contact'>/Contact</a><a href='http://gwparsa.ir/'>/Home</a><br><br>`; //input text here
+    type();   
+};
+
+const statusChanger = () => {
+    const nowDate = new Date().getHours();
+    if(discordStatus) {
+        return status = "Online on Discord!";
+    }else if(nowDate >= 12 && nowDate <= 23) {
+        return status = "I'm available! Contact me.";
+    }else{
+        return status = "I'm sleeping right now.";
+    }
+};
 
 function type() {
     captionEl.html(caption.substr(0, captionLength++));
@@ -21,7 +34,7 @@ function type() {
         captionLength = 0;
         caption = '';
     }
-}
+};
 
 function cursorAnimation() {
     $('#cursor').animate({
@@ -29,4 +42,4 @@ function cursorAnimation() {
     }, 'fast', 'swing').animate({
         opacity: 1
     }, 'fast', 'swing');
-}
+};
